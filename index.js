415f21e8ddc885447bc7ae2645d5048fc7b9ff29
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2023-02-23 09:35:43 -0500

    Complete and invoke the 'getDiscount()' function

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ function getPrices(taxBoolean) {
 }
 
 // Call getPrices()
-getPrices(true);
-console.log("\n");
-getPrices(false);
+// getPrices(true);
+// console.log("\n");
+// getPrices(false);
 
 // OUTPUTS
 /**
@@ -52,7 +52,6 @@ Dish: 'Italian pasta' => Price: $11.46
 Dish: 'Rice with veggies' => Price: $10.38
 Dish: 'Chicken with potatoes' => Price: $18.66
 Dish: 'Vegetarian Pizza' => Price: $7.74
-
 
 Dish: 'Italian pasta' => Price: $9.55
 Dish: 'Rice with veggies' => Price: $8.65
@@ -62,11 +61,50 @@ Dish: 'Vegetarian Pizza' => Price: $6.45
 
 
 // Implement getDiscount()
-function getDiscount() {
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+    let condition = typeof(guests) === 'number' && guests > 0 && guests < 30;
 
+    if (condition) {
+        let discount = 0;
+
+        if (guests < 5) {
+            discount = 5;
+        } else if (guests >= 5) {
+            discount = 10;
+        }
+        console.log(`Discount is: $${discount}`);
+    } else {
+        console.log("The second argument must be a number between 0 and 30");
+    }
 }
-// Call getDiscount()
 
+// Call getDiscount()
+getDiscount(true, 2);
+console.log("\n");
+getDiscount(false, 10);
+console.log("\n");
+getDiscount(true, true);
+
+// OUTPUTS
+/**
+Dish: 'Italian pasta' => Price: $11.46
+Dish: 'Rice with veggies' => Price: $10.38
+Dish: 'Chicken with potatoes' => Price: $18.66
+Dish: 'Vegetarian Pizza' => Price: $7.74
+Discount is: $5
+
+Dish: 'Italian pasta' => Price: $9.55
+Dish: 'Rice with veggies' => Price: $8.65
+Dish: 'Chicken with potatoes' => Price: $15.55
+Dish: 'Vegetarian Pizza' => Price: $6.45
+Discount is: $10
+
+Dish: 'Italian pasta' => Price: $11.46
+Dish: 'Rice with veggies' => Price: $10.38
+Dish: 'Chicken with potatoes' => Price: $18.66
+Dish: 'Vegetarian Pizza' => Price: $7.74
+The second argument must be a number between 0 and 30
+*/
 
 // module.exports = index;
-
