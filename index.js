bbd7de9e357ad34605ffc96a7222d6a684ddb6e7
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2023-02-23 09:19:19 -0500

    Create and invoke the 'getPrices()' function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Given variables
 const dishData = [
     {
@@ -16,18 +18,55 @@ const dishData = [
         name: "Vegetarian Pizza",
         price: 6.45
     },
-]
+];
+
 const tax = 1.20;
 
 
 // Implement getPrices()
-function getPrices() {
+function getPrices(taxBoolean) {
+    for (let dish of dishData) {
+        let finalPrice;
 
+        if (taxBoolean) {
+            finalPrice = dish.price * tax;
+        } else if (!taxBoolean) {
+            finalPrice = dish.price;
+        } else {
+            console.log("You need to pass a boolean to the getPrices call!");
+            return;
+        }
+
+        console.log(`Dish: '${dish.name}' => Price: $${finalPrice}`);
+    }
 }
+
+// Call getPrices()
+getPrices(true);
+console.log("\n");
+getPrices(false);
+
+// OUTPUTS
+/**
+Dish: 'Italian pasta' => Price: $11.46
+Dish: 'Rice with veggies' => Price: $10.38
+Dish: 'Chicken with potatoes' => Price: $18.66
+Dish: 'Vegetarian Pizza' => Price: $7.74
+
+
+Dish: 'Italian pasta' => Price: $9.55
+Dish: 'Rice with veggies' => Price: $8.65
+Dish: 'Chicken with potatoes' => Price: $15.55
+Dish: 'Vegetarian Pizza' => Price: $6.45
+*/
+
 
 // Implement getDiscount()
 function getDiscount() {
 
 }
-
 // Call getDiscount()
+
+
+// module.exports = index;
+
