11827b829658b5123ee52bac67aabc9b0b656353
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2023-02-23 21:03:49 -0500

    Comment out Jest testRunner instructions

--- index.test.js
+++ index.test.js
@@ -1,9 +1,11 @@
+/**
 const { default: testRunner } = require("jest-runner");
 
-const index = require("./index");
+const getDiscount = require(`./index`);
 
 test('Return discount according to price of lemon bought', () => {
-    expect(getDiscount(true, 10)).toBe(
-        // some outputs
+    expect(getDiscount(true, 2)).toBe(
+    //   .......
     );
 });
+*/
